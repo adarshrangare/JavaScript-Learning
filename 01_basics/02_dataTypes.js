a2e5c73ca7272f42a1dea141a30867f0d529015e
Adarsh Rangare <akrangare@gmail.com>
2023-09-19 16:29:16 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Adarsh")
 
 
-let name = "hitesh"
+let name = "adarsh"
 let age = 18
 let isLoggedIn = false
 let state;
