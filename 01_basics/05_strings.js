1627626cfa100d50e620f25f7b37c15b8b81a35f
Adarsh Rangare <akrangare@gmail.com>
2023-09-19 16:27:49 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "Adarsh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('adarsh-ad-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,13 +22,13 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   adarsh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://adarsh.com/adarsh%007rangare"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%007', '-'))
 
 console.log(url.includes('sundar'))
 
