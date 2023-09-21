28135add7303e9a3e6a3a0b527b9c9568c26e618
Adarsh Rangare <akrangare@gmail.com>
2023-09-21 20:50:25 +0530

    Update 02_scopes.js

--- 03_basics_Functions/02_scopes.js
+++ 03_basics_Functions/02_scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "adarsh"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "adarsh"
+    if (username === "adarsh") {
         const website = " youtube"
         // console.log(username + website);
     }
